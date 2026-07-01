8c23a1dd94d624113438d06945333498091cf9d0
Damin Sahu <daminsahu697@gmail.com>
2026-07-01 15:43:40 +0530

    build: migrate backend to native serverless api folder mapping

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [ react() ],
-  build: {
-    rollupOptions: {
-      // Tells Vite to completely ignore your backend script during compilation
-      external: [ './server.js' ]
-    }
-  }
 });
